799dfe4a8285f002985896a33a4238210bffbed6
Namgay-Thinley <07230037.sherubtse@rub.edu.bt>
2025-04-02 10:56:33 +0600

    Practical 4

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const indexRoutes = require('./routes/index');
 const authRoutes = require('./routes/authRoutes');
 const homeRoutes = require('./routes/homeRoutes'); 
 const profileRoutes = require('./routes/profileRoutes');
+const bookRoutes = require("./routes/bookRoutes");
 
 //load environment variables
 dotenv.config();
@@ -41,8 +42,9 @@ app.use('/components', express.static(path.join(__dirname, 'views/components')))
 // routes
 app.use('/', indexRoutes);
 app.use('/auth', authRoutes);
-app.use('/', homeRoutes); // Add this line
+app.use('/', homeRoutes); 
 app.use('/', profileRoutes);
+app.use("/", bookRoutes);
 
 //for db connection
 app.get('/db-test', async (req, res) => {
